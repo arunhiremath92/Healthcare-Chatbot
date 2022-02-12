75844a0572d352cc07eb365fa552045a645e864d
Arun Hiremath <hiremaar@Aruns-MacBook-Pro.local>
2022-02-11 20:57:16 -0800

    ground zero

--- webapp/healthcare/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('', include('platform_health.urls')),
-    path('admin/', admin.site.urls),
-    path("login/", include("log_reg.urls")),
-]
