92bd8832a07ab9743468d95b3e62cd43e1ce5c07
Arun Hiremath <arunhiremath92@gmail.com>
2022-02-14 20:04:57 -0800

    added server build files

--- webapp/backend/healthcare_platform/healthcare_platform/settings.py
+++ webapp/backend/healthcare_platform/healthcare_platform/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%1pv!t0s@l86@8oo*$xlk7*#w$o*be5z(g0r6alk!@bp7rf#8_'
+import os
+SECRET_KEY = os.environ.get("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
